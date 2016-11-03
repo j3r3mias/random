80f7a3ef54b82806f9ecc03f9bbc711822658bb2
Jeremias Moreira Gomes <j3r3miasmg@gmail.com>
2016-11-03 15:22:15 -0200

    Videogame emulators and drivers

--- format-setups/format-debian.sh
+++ format-setups/format-debian.sh
@@ -8,9 +8,9 @@ zbar-tools g++ g++-6 gcc-6 git curl vinetto skype pdf-presenter-console \
 libpcap0.8-dev cmake strace ltrace smplayer alsa-utils network-manager \
 python-software-properties apt-files gimp inkscape chkconfig htop \
 libgtkmm.3.0-dev libssl-dev gettext libarchive-dev hexchat cmake-curses-gui \
-torbrowser-launcher)
+torbrowser-launcher higan mame xboxdrv)
 
-piplist=(hashlib jedi pwn xortool)
+piplist=(hashlib jedi pwn xortool ds4drv)
 
 function check()
 {
