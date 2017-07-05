ee795f3b84ff552137d9eb911573af71542e3bf4
Jeremias Moreira Gomes <j3r3mias@localhost.localdomain>
2017-07-04 22:27:41 -0300

    Fix some bugs with PS1 and more tools included.

--- format-setups/format-debian.sh
+++ format-setups/format-debian.sh
@@ -5,14 +5,16 @@ user="j3r3mias"
 homepath="/home/$user/"
 
 list=(build-essential autoconf libtool pkg-config python-dev python3-dev \
-python-pip texlive-full terminator vim vim-gtk iptraf audacity vlc mediainfo \
-unrar wxhexeditor ht bless binwalk wireshark aircrack-ng wifite nmap hydra \
-zbar-tools g++ g++-6 gcc-6 git curl vinetto skype pdf-presenter-console \
-libpcap0.8-dev cmake strace ltrace smplayer alsa-utils network-manager \
-python-software-properties apt-files gimp inkscape chkconfig htop \
-libgtkmm.3.0-dev libssl-dev gettext libarchive-devsudo cmake-curses-gui \
-hexchat dcfldd torbrowser-launcher higan mame xboxdrv lib32stdc++6 mtr-tiny\ 
-)
+    python-pip texlive-full terminator vim vim-gtk iptraf audacity vlc \
+    mediainfo unrar wxhexeditor ht bless binwalk wireshark aircrack-ng wifite \
+    nmap hydra zbar-tools g++ g++-6 gcc-6 git curl vinetto skype \
+    pdf-presenter-console libpcap0.8-dev cmake strace ltrace smplayer \
+    alsa-utils network-manager python-software-properties apt-files gimp \
+    inkscape chkconfig htop libgtkmm.3.0-dev libssl-dev gettext \
+    libarchive-devsudo cmake-curses-gui hexchat dcfldd torbrowser-launcher \
+    higan mame xboxdrv lib32stdc++6 mtr-tiny dkms virtualbox cups exiftools \
+    steghide imagemagick lzip apache2 ltrace deluge jd-gui spotify-client \
+    bsdiff wine printer-driver-pxljr  )
 
 piplist=(hashlib jedi pwn xortool hashid)
 
@@ -78,114 +80,116 @@ fi
 echo "Let's go!"
 version=$(uname -a)
 
-# if [[ $version == *Ubuntu* ]]
-# then 
-#     echo " [+] Ubuntu system. No need to add repositories."
-# elif [[ $version == *kali* ]]
-# then
-#     echo " [+] Kali system. Adding some repositories."
-#     $homepath="/etc/apt/sources.list"
-#     echo -n "" > $$homepath
-#     echo "deb http://kali.cs.nctu.edu.tw/kali kali-dev main contrib non-free" >> $$homepath
-#     echo "deb http://kali.cs.nctu.edu.tw/kali kali-dev main/debian-installer" >> $$homepath
-#     echo "deb-src http://kali.cs.nctu.edu.tw/kali kali-dev main contrib non-free" >> $$homepath
-#     echo "deb http://kali.cs.nctu.edu.tw/kali kali main contrib non-free" >> $$homepath
-#     echo "deb http://kali.cs.nctu.edu.tw/kali kali main/debian-installer" >> $$homepath
-#     echo "deb-src http://kali.cs.nctu.edu.tw/kali kali main contrib non-free" >> $$homepath
-#     echo "deb http://kali.cs.nctu.edu.tw/kali-security kali/updates main contrib non-free" >> $$homepath
-#     echo "deb-src http://kali.cs.nctu.edu.tw/kali-security kali/updates main contrib non-free" >> $$homepath
-#     echo "deb http://kali.cs.nctu.edu.tw/kali kali-bleeding-edge main" >> $$homepath
-# else
-#     echo " [+] Unknow system."
-# fi
-# 
-# echo " [+] Updating repositories."
-# apt-get update
-# echo " [+] Upgrading repositories."
-# apt-get upgrade -y
-# echo " [+] Updating repositories."
-# apt-get update
-# updatedb
-# 
-# for current in ${list[@]}
-# do
-#     echo " [+] Checking $current."
-#     check $current
-# done
-# 
-# for current in ${piplist[@]}
-# do
-#     echo " [+] Checking $current."
-#     pipcheck $current
-# done
-# 
-# ### Automatic MACCHANGER
-# file=script-macchanger
-# echo "#!/bin/bash" > $file
-# echo "" >> $file
-# echo "ifconfig eth0 down" >> $file
-# echo "ifconfig wlan0 down" >> $file
-# echo "macchanger -r eth0" >> $file
-# echo "macchanger -r wlan0" >> $file
-# echo "ifconfig eth0 up" >> $file
-# echo "ifconfig wlan0 up" >> $file
-# chmod +x $file
-# mv $file /usr/bin/
-# cd $homepath
+if [[ $version == *Ubuntu* ]]
+then 
+    echo " [+] Ubuntu system. No need to add repositories."
+elif [[ $version == *kali* ]]
+then
+    echo " [+] Kali system. Adding some repositories."
+    $homepath="/etc/apt/sources.list"
+    echo -n "" > $$homepath
+    echo "deb http://kali.cs.nctu.edu.tw/kali kali-dev main contrib non-free" >> $$homepath
+    echo "deb http://kali.cs.nctu.edu.tw/kali kali-dev main/debian-installer" >> $$homepath
+    echo "deb-src http://kali.cs.nctu.edu.tw/kali kali-dev main contrib non-free" >> $$homepath
+    echo "deb http://kali.cs.nctu.edu.tw/kali kali main contrib non-free" >> $$homepath
+    echo "deb http://kali.cs.nctu.edu.tw/kali kali main/debian-installer" >> $$homepath
+    echo "deb-src http://kali.cs.nctu.edu.tw/kali kali main contrib non-free" >> $$homepath
+    echo "deb http://kali.cs.nctu.edu.tw/kali-security kali/updates main contrib non-free" >> $$homepath
+    echo "deb-src http://kali.cs.nctu.edu.tw/kali-security kali/updates main contrib non-free" >> $$homepath
+    echo "deb http://kali.cs.nctu.edu.tw/kali kali-bleeding-edge main" >> $$homepath
+    echo "deb http://repository.spotify.com stable non-free" >> $$homepath
+else
+    echo " [+] Unknow system."
+fi
 
-# ### PATHOGEN FOR VIM
-# echo " [+] Installing pathogen for vim"
-# mkdir -p $homepath/.vim/autoload $homepath/.vim/bundle
-# curl -LSso $homepath/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo " [+] Updating repositories."
+apt-get update
+echo " [+] Upgrading repositories."
+apt-get upgrade -y
+echo " [+] Updating repositories."
+apt-get update
+updatedb
 
-## ### CONFIGURATIONS FOR VIM
-## echo " [+] Creating .vimrc"
-## 
-## cd $homepath
-## file=.vimrc
-## echo "execute pathogen#infect()" > $file
-## echo "    call pathogen#helptags()" >> $file
-## echo "    filetype plugin indent on" >> $file
-## echo "    syntax on \"Para utilizar com o tema solarized" >> $file
-## echo "    set showmatch \"mostra caracteres ( { [ quando fechados" >> $file
-## echo "    set textwidth=80 \"largura do texto" >> $file
-## echo "    set nowrap  \"sem wrap (quebra de linha)" >> $file
-## echo "    set mouse=a \"habilita todas as acoes do mouse" >> $file
-## echo "    set nu \"numeracao de linhas" >> $file
-## echo "    set ts=4 \"Seta onde o tab para" >> $file
-## echo "    set sw=4 \"largura do tab" >> $file
-## echo "    set et \"espacos em vez de tab" >> $file
-## echo "    \"set spell spelllang=pt\"" >> $file
-## echo "    set nospell" >> $file
-## echo "    set background=light" >> $file
-## echo "    \"set background=dark" >> $file
-## echo "    let g:ycm_min_num_of_chars_for_completion = 1" >> $file
-## echo "    set clipboard=unnamedplus \"Permite copiar direto para o clipboard" >> $file
-## echo "    set laststatus=2" >> $file
-## echo "    set t_Co=256" >> $file
-## echo "    let g:airline_powerline_fonts=1" >> $file
-## echo "    set relativenumber" >> $file
-## echo "    highlight OverLength ctermbg=red ctermfg=white guibg=#592929" >> $file
-## echo "    match OverLength /\%81v.*/" >> $file
-## 
-## ### YOUCOMPLETEME
-## echo " [+] Installing Plugins for Vim"
-## git clone https://github.com/Valloric/YouCompleteMe.git $homepath/.vim/bundle/YouCompleteMe
-## cd $homepath/.vim/bundle/YouCompleteMe/
-## git submodule update --init --recursive
-## ./install.sh
+for current in ${list[@]}
+do
+    echo " [+] Checking $current."
+    check $current
+done
 
+for current in ${piplist[@]}
+do
+    echo " [+] Checking $current."
+    pipcheck $current
+done
+
+### Automatic MACCHANGER
+file=script-macchanger
+echo "#!/bin/bash" > $file
+echo "" >> $file
+echo "ifconfig eth0 down" >> $file
+echo "ifconfig wlan0 down" >> $file
+echo "macchanger -r eth0" >> $file
+echo "macchanger -r wlan0" >> $file
+echo "ifconfig eth0 up" >> $file
+echo "ifconfig wlan0 up" >> $file
+chmod +x $file
+mv $file /usr/bin/
 cd $homepath
+
+### PATHOGEN FOR VIM
+echo " [+] Installing pathogen for vim"
+mkdir -p $homepath/.vim/autoload $homepath/.vim/bundle
+curl -LSso $homepath/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+ ### CONFIGURATIONS FOR VIM
+ echo " [+] Creating .vimrc"
+ 
+ cd $homepath
+ file=.vimrc
+ echo "execute pathogen#infect()" > $file
+ echo "    call pathogen#helptags()" >> $file
+ echo "    filetype plugin indent on" >> $file
+ echo "    syntax on \"Para utilizar com o tema solarized" >> $file
+ echo "    set showmatch \"mostra caracteres ( { [ quando fechados" >> $file
+ echo "    set textwidth=80 \"largura do texto" >> $file
+ echo "    set nowrap  \"sem wrap (quebra de linha)" >> $file
+ echo "    set mouse=a \"habilita todas as acoes do mouse" >> $file
+ echo "    set nu \"numeracao de linhas" >> $file
+ echo "    set ts=4 \"Seta onde o tab para" >> $file
+ echo "    set sw=4 \"largura do tab" >> $file
+ echo "    set et \"espacos em vez de tab" >> $file
+ echo "    \"set spell spelllang=pt\"" >> $file
+ echo "    set nospell" >> $file
+ echo "    set background=light" >> $file
+ echo "    \"set background=dark" >> $file
+ echo "    let g:ycm_min_num_of_chars_for_completion = 1" >> $file
+ echo "    set clipboard=unnamedplus \"Permite copiar direto para o clipboard" >> $file
+ echo "    set laststatus=2" >> $file
+ echo "    set t_Co=256" >> $file
+ echo "    let g:airline_powerline_fonts=1" >> $file
+ echo "    set relativenumber" >> $file
+ echo "    highlight OverLength ctermbg=red ctermfg=white guibg=#592929" >> $file
+ echo "    match OverLength /\%81v.*/" >> $file
+ 
+ ### YOUCOMPLETEME
+ echo " [+] Installing Plugins for Vim"
+ git clone https://github.com/Valloric/YouCompleteMe.git $homepath/.vim/bundle/YouCompleteMe
+ cd $homepath/.vim/bundle/YouCompleteMe/
+ git submodule update --init --recursive
+ ./install.sh
+
+cd $homepath/.vim/
+git init
 echo '1'
-git submodule add https://github.com/scrooloose/syntastic.git $homepath/.vim/bundle/syntastic
+git submodule add https://github.com/scrooloose/syntastic.git bundle/syntastic
 echo '2'
-git submodule add https://github.com/jiangmiao/auto-pairs.git $homepath/.vim/bundle/auto-pairs
-git submodule add https://github.com/vim-scripts/The-NERD-tree.git $homepath/.vim/bundle/nerdtree
-git submodule add https://github.com/bling/vim-airline.git $homepath/.vim/bundle/vim-airline
-git submodule add https://github.com/altercation/vim-colors-solarized.git $homepath/.vim/bundle/vim-colors-solarized
-git submodule add http://github.com/tpope/vim-fugitive.git $homepath/.vim/bundle/fugitive
+git submodule add https://github.com/jiangmiao/auto-pairs.git bundle/auto-pairs
+git submodule add https://github.com/vim-scripts/The-NERD-tree.git bundle/nerdtree
+git submodule add https://github.com/bling/vim-airline.git bundle/vim-airline
+git submodule add https://github.com/altercation/vim-colors-solarized.git bundle/vim-colors-solarized
+git submodule add http://github.com/tpope/vim-fugitive.git bundle/fugitive
 vim -u NONE -c "helptags vim-fugitive/doc" -c q
-git submodule add https://github.com/davidhalter/jedi-vim.git $homepath/.vim/bundle/jedi-vim
+git submodule add https://github.com/davidhalter/jedi-vim.git bundle/jedi-vim
 git submodule init
 git submodule update
 git submodule foreach git submodule init
@@ -193,108 +197,116 @@ git submodule foreach git submodule update
 
 cd $homepath
 
-# ### COWPATTY
-# echo " [+] Checking cowpatty: "
-# exec 2> /dev/null
-# status=$(apt list --installed | grep 'installed' | grep cowpatty | \
-#        tail -n 1 | awk -F/ '{print $1}')
-# if [[ ! -z $status ]]
-# then
-#     echo -n "found."
-# else
-#     echo " [!] Installing cowpatty (in opt): "
-#     cd /opt/
-#     wget http://www.willhackforsushi.com/code/cowpatty/4.6/cowpatty-4.6.tgz
-#     tar xvf cowpatty-4.6.tgz
-#     rm -rf cowpatty-4.6.tgz
-#     cd cowpatty-4.6
-#     make -j3
-#     cp cowpatty /usr/bin/
-#     echo -n "done!"
-# fi
-# 
-# ## TELEGRAM
-# echo " [+] Installing Telegram (desktop)."
-# cd /opt/
-# wget https://updates.tdesktop.com/tlinux/tsetup.0.10.11.tar.xz
-# tar xvf tsetup.0.10.11.tar.xz
-# rm -rf tsetup.0.10.11.tar.xz
-# cd Telegram
-# mv Telegram /usr/bin
-# cd /opt/
-# rm -rf Telegram/
-# echo "[+] Creating telegram alias."
-# echo '' >> $homepath/.bashrc 
-# echo '# Telegram alias' >> $homepath/.bashrc 
-# echo "alias telegram='nohup Telegram &'" >> $homepath/.bashrc 
-# echo "alias Telegram='nohup Telegram &'" >> $homepath/.bashrc 
-# 
-# echo "" >> /.bashrc
-# echo "alias Telegram='nohup Telegram &'" >> /.bashrc
-# echo "alias telegram='nohup Telegram &'" >> /.bashrc
-# 
-# ### PULSEAUDIO
-# echo " [+] Fixing pulseaudio."
-# killall -9 pulseaudio
-# systemctl --user enable pulseaudio && systemctl --user start pulseaudio
-# 
-# ### PEDA
-# echo " [+] Downloading and installing peda."
-# cd /opt/
-# git clone https://github.com/longld/peda.git peda
-# echo “source peda/peda.py” > $homepath/.gdbinit
-# 
-# ### Installing Grub-Customizer
-# echo " [+] Downloading and installing Grub-Customizer."
-# cd /opt/
-# wget https://launchpadlibrarian.net/172968333/grub-customizer_4.0.6.tar.gz
-# tar xvf grub-customi*
-# cd grub-customi*
-# cmake . && make -j3
-# make install
-# 
-# ### Lid configuration
-# echo " [+] Creating lid configurations for screen close."
-# file=lid
-# echo -n '' > $file
-# echo '#!/bin/bash' >> $file
-# echo '# Getting first parameter' >> $file
-# echo 'status=$1' >> $file
-# echo '' >> $file
-# echo 'file=/etc/systemd/logind.conf' >> $file
-# echo '' >> $file
-# echo '# Uncomment the line' >> $file
-# echo 'sed "s/\#HandleLidSwitch=.*/HandleLidSwitch=ignore/g" $file > .logind.conf' >> $file
-# echo '' >> $file
-# echo 'if [[ $status == *off* ]]' >> $file
-# echo 'then' >> $file
-# echo '    echo "Lid suspend deactivated."' >> $file
-# echo '    new="kexec"' >> $file
-# echo 'elif [[ $status == *on* ]]' >> $file
-# echo 'then' >> $file
-# echo '    echo "Lid suspend activated."' >> $file
-# echo '    new="suspend"' >> $file
-# echo 'else' >> $file
-# echo '    current=$(cat $file | grep "HandleLidSwitch=" | cut -d '=' -f 2)' >> $file
-# echo '    if [[ $current == *kexec* ]]' >> $file
-# echo '    then' >> $file
-# echo '        echo "Lid suspend deactivated."' >> $file
-# echo '        new="suspend"' >> $file
-# echo '    else' >> $file
-# echo '        echo "Lid suspend activated."' >> $file
-# echo '        new="kexec"' >> $file
-# echo '    fi' >> $file
-# echo 'fi' >> $file
-# echo 'sed "s/HandleLidSwitch=.*/HandleLidSwitch=$new/g" $file > .logind.conf' >> $file
-# echo 'mv .logind.conf $file' >> $file
-# echo 'echo "You need to restart your computer to apply the new configs (bug of' >> $file
-# echo 'systemctl)"' >> $file
-# chmod +x $file
-# mv lid /usr/bin/
-# echo " [+] Setting suspend off."
-# lid off
-# 
-# echo " [+] Creating new directory path view."
-# echo '' >> $homepath/.bashrc 
-# echo 'export PS1="$(whoami):${PWD/*\//}# "' >> $homepath/.bashrc 
-# echo '' >> $homepath/.bashrc 
+### COWPATTY
+echo " [+] Checking cowpatty: "
+exec 2> /dev/null
+status=$(apt list --installed | grep 'installed' | grep cowpatty | \
+       tail -n 1 | awk -F/ '{print $1}')
+if [[ ! -z $status ]]
+then
+    echo -n "found."
+else
+    echo " [!] Installing cowpatty (in opt): "
+    cd /opt/
+    wget http://www.willhackforsushi.com/code/cowpatty/4.6/cowpatty-4.6.tgz
+    tar xvf cowpatty-4.6.tgz
+    rm -rf cowpatty-4.6.tgz
+    cd cowpatty-4.6
+    make -j3
+    cp cowpatty /usr/bin/
+    echo -n "done!"
+fi
+
+## TELEGRAM
+echo " [+] Installing Telegram (desktop)."
+telegram=telegram.tar.xz
+cd /opt/
+curl -LSso $telegram https://telegram.org/dl/desktop/linux
+tar xvf $telegram
+rm -rf $telegram
+cd Telegram
+mv Telegram /usr/bin
+cd /opt/
+rm -rf Telegram/
+echo "[+] Creating telegram alias."
+echo '' >> $homepath/.bashrc 
+echo '# Telegram alias' >> $homepath/.bashrc 
+echo "alias telegram='nohup Telegram &'" >> $homepath/.bashrc 
+echo "alias Telegram='nohup Telegram &'" >> $homepath/.bashrc 
+
+echo "" >> /.bashrc
+echo "alias Telegram='nohup Telegram &'" >> /.bashrc
+echo "alias telegram='nohup Telegram &'" >> /.bashrc
+
+### PULSEAUDIO
+echo " [+] Fixing pulseaudio."
+killall -9 pulseaudio
+systemctl --user enable pulseaudio && systemctl --user start pulseaudio
+
+### PEDA
+echo " [+] Downloading and installing peda."
+cd /opt/
+git clone https://github.com/longld/peda.git peda
+echo “source peda/peda.py” > $homepath/.gdbinit
+
+### Installing Grub-Customizer
+echo " [+] Downloading and installing Grub-Customizer."
+cd /opt/
+wget https://launchpadlibrarian.net/172968333/grub-customizer_4.0.6.tar.gz
+tar xvf grub-customi*
+cd grub-customi*
+cmake . && make -j3
+make install
+
+### Lid configuration
+echo " [+] Creating lid configurations for screen close."
+file=lid
+echo -n '' > $file
+echo '#!/bin/bash' >> $file
+echo '# Getting first parameter' >> $file
+echo 'status=$1' >> $file
+echo '' >> $file
+echo 'file=/etc/systemd/logind.conf' >> $file
+echo '' >> $file
+echo '# Uncomment the line' >> $file
+echo 'sed "s/\#HandleLidSwitch=.*/HandleLidSwitch=ignore/g" $file > .logind.conf' >> $file
+echo '' >> $file
+echo 'if [[ $status == *off* ]]' >> $file
+echo 'then' >> $file
+echo '    echo "Lid suspend deactivated."' >> $file
+echo '    new="kexec"' >> $file
+echo 'elif [[ $status == *on* ]]' >> $file
+echo 'then' >> $file
+echo '    echo "Lid suspend activated."' >> $file
+echo '    new="suspend"' >> $file
+echo 'else' >> $file
+echo '    current=$(cat $file | grep "HandleLidSwitch=" | cut -d '=' -f 2)' >> $file
+echo '    if [[ $current == *kexec* ]]' >> $file
+echo '    then' >> $file
+echo '        echo "Lid suspend deactivated."' >> $file
+echo '        new="suspend"' >> $file
+echo '    else' >> $file
+echo '        echo "Lid suspend activated."' >> $file
+echo '        new="kexec"' >> $file
+echo '    fi' >> $file
+echo 'fi' >> $file
+echo 'sed "s/HandleLidSwitch=.*/HandleLidSwitch=$new/g" $file > .logind.conf' >> $file
+echo 'mv .logind.conf $file' >> $file
+echo 'echo "You need to restart your computer to apply the new configs (bug of' >> $file
+echo 'systemctl)"' >> $file
+chmod +x $file
+mv lid /usr/bin/
+echo " [+] Setting suspend off."
+lid off
+
+echo " [+] Creating new directory path view."
+echo '' >> $homepath/.bashrc 
+echo 'export PS1="\u@\h:[\W]# "' >> $homepath/.bashrc 
+echo '' >> $homepath/.bashrc 
+
+echo " [+] Increasing history size."
+sed -i 's/\(HISTSIZE=\).*/\1100000/;s/\(HISTFILESIZE=\).*/\1200000/' $homepath/.bashrc
+
+echo "     OK"
+echo ""
+echo " [!] All done."
