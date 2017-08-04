19dc2b970d2780ffacbe97773c8a2277d2b161c1
Jeremias Moreira Gomes <j3r3miasmg@gmail.com>
2017-08-04 19:40:42 -0300

    Malfunction occasioned from @atcasanova pull request.

--- quine.cpp
+++ quine.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 static char special[] = "\n\"\\\t";
 int main() {
-	char *text="#include<stdio.h>%cstatic char special[] = %c%cn%c%c%c%c%ct%c;%cint main() {%c%cchar *text=%c%s%c;%c%cprintf(text,special[0],special[1],special[2],special[2],special[1],special[2],special[2],special[2],special[1],special[0],special[0],special[3],special[1],text,special[1],special[0],special[3],special[0],special[3],special[0],special[0],special[0],special[0],special[0],special[0],special[0],special[0],special[0]);%c%creturn 0;%c}%c/**%c *%c * Create a program that write yourself%c *%c * @author jeremias%c *%c */%c";
+	char *text="#include<stdio.h>%cstatic char special[] = %c%cn%c%c%c%c%ct%c;%cint main() {%c%cchar *text=%c%s%c;%c%cprintf(text,special[0],special[1],special[2],special[2],special[1],special[2],special[2],special[2],special[1],special[0],special[0],special[3],special[1],text,special[1],special[0],special[3],special[0],special[3],special[0],special[0],special[0],special[0],special[0],special[0],special[0],special[0],special[0]);%c%creturn 0;%c}%c/**%c *%c * Create a program that write itself%c *%c * @author jeremias%c *%c */%c";
 	printf(text,special[0],special[1],special[2],special[2],special[1],special[2],special[2],special[2],special[1],special[0],special[0],special[3],special[1],text,special[1],special[0],special[3],special[0],special[3],special[0],special[0],special[0],special[0],special[0],special[0],special[0],special[0],special[0]);
 	return 0;
 }
